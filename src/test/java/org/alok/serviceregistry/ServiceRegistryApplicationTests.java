d41c99997ce768bdf3266e925a5d6ed2d669a3a3
aloksingh <alokkpsingh123@gmail.com>
2023-12-06 22:42:04 +0530

    service registry

---
+++ src/test/java/org/alok/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alok.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
